953625dbcb5161ea8e010ea9cb143736c9cd337c
Amila Perera <amilaperera97@gmail.com>
2022-05-17 12:10:04 +0100

    Code refactor

--- src/main/java/com/techbooker/sm/util/dto/CommonDataDto.java
+++ src/main/java/com/techbooker/sm/util/dto/CommonDataDto.java
@@ -1,8 +1,10 @@
 package com.techbooker.sm.util.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CommonDataDto {
     private UserProfileDto user;
 }
--- src/main/java/com/techbooker/sm/util/dto/UserProfileDto.java
+++ src/main/java/com/techbooker/sm/util/dto/UserProfileDto.java
@@ -1,8 +1,10 @@
 package com.techbooker.sm.util.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserProfileDto {
     private String username;
     private Role role;
